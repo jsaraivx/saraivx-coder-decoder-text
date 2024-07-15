d9b62388013a783210e8fd7789cd0ae032b7a5a7
João Saraiva <joaogabrielssaraiva@gmail.com>
2024-07-15 17:19:06 -0300

    fix

--- script.js
+++ script.js
@@ -5,48 +5,45 @@
 // "o" == "ober"
 // "u" == "ufat"
 
-function encryptText(inputedText)
-{
-    textInputed = document.getElementById('inputText').value;
-    codedText = textInputed;
-    codedText = codedText.replaceAll('e', 'enter');
-    codedText = codedText.replaceAll('i', 'imes');
-    codedText = codedText.replaceAll('a', 'ai');
-    codedText = codedText.replaceAll('o', 'ober');
-    codedText = codedText.replaceAll('u', 'ufat');
-    console.log(`the decoded text is: ${codedText}`);
-    //codification output area and inject HTML 
-    document.getElementById('returnArea').innerHTML = codedText;
-    resultText = codedText;
-    return resultText;
+function encryptText(inputedText) {
+  textInputed = document.getElementById("inputText").value;
+  codedText = textInputed;
+  codedText = codedText.replaceAll("e", "enter");
+  codedText = codedText.replaceAll("i", "imes");
+  codedText = codedText.replaceAll("a", "ai");
+  codedText = codedText.replaceAll("o", "ober");
+  codedText = codedText.replaceAll("u", "ufat");
+  console.log(`the decoded text is: ${codedText}`);
+  //codification output area and inject HTML
+  document.getElementById("returnArea").innerHTML = codedText;
+  resultText = codedText;
+  return resultText;
 }
 
-function unencryptText(encryptedText)
-{    
-    //codification area
-    textInputed = document.getElementById('inputText').value;
-    decodedText = textInputed;
-    decodedText = decodedText.replaceAll('enter', 'e');
-    decodedText = decodedText.replaceAll('imes', 'i');
-    decodedText = decodedText.replaceAll('ai', 'a');
-    decodedText = decodedText.replaceAll('ober', 'o');
-    decodedText = decodedText.replaceAll('ufat', 'u');
-    console.log(`the decoded text is: ${decodedText}`);
-    //codification output area and inject HTML 
-    document.getElementById('returnArea').innerHTML = decodedText;
-    resultText = decodedText
-    return resultText;
-    
+function unencryptText(encryptedText) {
+  //codification area
+  textInputed = document.getElementById("inputText").value;
+  decodedText = textInputed;
+  decodedText = decodedText.replaceAll("enter", "e");
+  decodedText = decodedText.replaceAll("imes", "i");
+  decodedText = decodedText.replaceAll("ai", "a");
+  decodedText = decodedText.replaceAll("ober", "o");
+  decodedText = decodedText.replaceAll("ufat", "u");
+  console.log(`the decoded text is: ${decodedText}`);
+  //codification output area and inject HTML
+  document.getElementById("returnArea").innerHTML = decodedText;
+  resultText = decodedText;
+  return resultText;
 }
-function copyToClipboard()
-{
-    let textResulted = document.getElementById('inputText').value;
-    textResulted.select();
-    textResulted.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(textResulted.value);
-    alert("");
+function copyToClipboard() {
+  let textResulted = document.getElementById("inputText").value;
+  textResulted.select();
+  textResulted.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(textResulted.value);
+  alert("");
 }
 
-function copyButton(){ //show copy button 
-    //buttonCopy = document.getElementById('copyClipboard').removeAttribute('hidden');
+function copyButton() {
+  //show copy button
+  //buttonCopy = document.getElementById('copyClipboard').removeAttribute('hidden');
 }
